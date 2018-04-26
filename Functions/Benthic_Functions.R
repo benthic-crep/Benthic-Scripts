c48f7f8f08a1cd440754c9788990482fefa56b51
cscouch <32000438+cscouch@users.noreply.github.com>
2018-04-26 13:11:12 -1000

    updates to Benthic functions

--- Functions/Benthic_Functions.R
+++ Functions/Benthic_Functions.R
@@ -140,6 +140,8 @@ Calc_ColDen_Transect<-function(data, grouping_field="GENUS_CODE"){
   out<-subset(out,select=-c(TRANSECTAREA))#remove transect area column
   colnames(out)[which(colnames(out) == 'GROUP')] <- grouping_field #change group to whatever your grouping field is.
   
+  
+  #Add in proportion occurance- add colomn 0 or 1
   return(out)
 }
 
@@ -174,7 +176,7 @@ Calc_ColMetric_Transect<-function(data, grouping_field="S_ORDER",pool_fields=c("
 
 Calc_RDden_Transect<-function(data, grouping_field="S_ORDER"){
 scl<-subset(data,COLONYLENGTH>5&S_ORDER=="Scleractinia")
-  
+
 rd1<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ RD1, value.var="RD1",length,fill=0);names(rd1)<-gsub("DZGN","DZGNS",names(rd1),fixed = TRUE)#; rd1<-rd1[,-c(1:3)]
 rd2<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ RD2, value.var="RD2",length,fill=0) ;names(rd2)<-gsub("DZGN","DZGNS",names(rd2),fixed = TRUE)#; rd2<-rd2[,-c(1:3)]
 rd3<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ GENRD1, value.var="GENRD1",length,fill=0);colnames(rd3)[5:ncol(rd3)]<-paste("All",colnames(rd3[,c(5:ncol(rd3))]),sep="_") 
@@ -195,7 +197,7 @@ head(allrd)
 
 #Sum identically named columns and remove the no data column
 allrd2<-as.data.frame(sapply(unique(colnames(allrd)), 
-                             function(x) rowSums(allrd[, colnames(allrd) == x, drop = FALSE])));allrd2<-allrd2[,!(colnames(allrd2) =="All_NODATA")]
+                             function(x) rowSums(allrd[, colnames(allrd) == x, drop = FALSE])));allrd2<-allrd2[,!(colnames(allrd2) =="All_NONE")]
 
 #merge data with colony level metadata and sum conditions by transect and taxoncode
 allrd3<-merge(survey_colony,allrd2, by="COLONYID")
@@ -209,7 +211,7 @@ out1<-ddply(longsum, .(SITE,SITEVISITID,TRANSECT,RDCond), #calc total colonies b
            RDabun=sum(RDabun))
 out1$GROUP<-"SSSS"; out1 <- out1[c(1,2,3,6,4,5)] #add total colony code
 a<-rbind(longsum,out1)
-a<-subset(a,RDCond!="NODATA")
+a<-subset(a,RDCond!="NONE")
 
 #Convert back to wide format
 abun<-dcast(a, formula=SITEVISITID +SITE + TRANSECT+GROUP~ RDCond, value.var="RDabun",sum,fill=0)
@@ -231,6 +233,73 @@ colnames(out)[which(colnames(out) == 'GROUP')] <- grouping_field #change group t
 return(out)
 }
 
+#MESS WITH THIS FUNCTION 
+#This function calculate abundance of recent dead conditions by transect and taxonomic group
+#Transform RD1 and RD2 from long to wide format, remove other site AND TRANSECT level info
+
+Calc_RDden_Transect<-function(data, grouping_field="S_ORDER"){
+  scl<-subset(data,COLONYLENGTH>5&S_ORDER=="Scleractinia")
+  
+  awd2$GENRD1<-paste(awd2$GENRD1,"S",sep="");awd2$GENRD2<-paste(awd2$GENRD2,"S",sep="")
+  long <- gather(awd2, RDCond,abun, names(awd2[c(32,33,35,36)]), factor_key=TRUE) #convert wide to long format by condition
+  # 
+  #add in a check to make sure that RD1 and RD2 aren't the same
+  
+  rd1<-dcast(awd2, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ RD1, value.var="RD1",length,fill=0);names(rd1)<-gsub("DZGN","DZGNS",names(rd1),fixed = TRUE)#; rd1<-rd1[,-c(1:3)]
+  rd2<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ RD2, value.var="RD2",length,fill=0) ;names(rd2)<-gsub("DZGN","DZGNS",names(rd2),fixed = TRUE)#; rd2<-rd2[,-c(1:3)]
+  rd3<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ GENRD1, value.var="GENRD1",length,fill=0);colnames(rd3)[5:ncol(rd3)]<-paste("All",colnames(rd3[,c(5:ncol(rd3))]),sep="_") 
+  rd4<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ GENRD2, value.var="GENRD2",length,fill=0);colnames(rd4)[5:ncol(rd4)]<-paste("All",colnames(rd4[,c(5:ncol(rd4))]),sep="_") 
+  
+  #merge all dataframes together
+  a<-merge(rd1,rd2,by=c("SITEVISITID","SITE","TRANSECT","COLONYID"),all=TRUE)
+  b<-merge(a,rd3,by=c("SITEVISITID","SITE","TRANSECT","COLONYID"),all=TRUE)
+  allrd<-merge(b,rd4,by=c("SITEVISITID","SITE","TRANSECT","COLONYID"),all=TRUE)
+  
+  allrd<-allrd[,-c(1:3)] #remove all metadata except colonyid
+  
+  #R will add .x and .y to column names because there are columns that are identically names
+  # this will remove .x and .y so that we can sum identifically named columns
+  names(allrd)<-gsub(".x","",names(allrd),fixed = TRUE)
+  names(allrd)<-gsub(".y","",names(allrd),fixed = TRUE)
+  head(allrd)
+  
+  #Sum identically named columns and remove the no data column
+  allrd2<-as.data.frame(sapply(unique(colnames(allrd)), 
+                               function(x) rowSums(allrd[, colnames(allrd) == x, drop = FALSE])));allrd2<-allrd2[,!(colnames(allrd2) =="All_NONE")]
+  
+  #merge data with colony level metadata and sum conditions by transect and taxoncode
+  allrd3<-merge(survey_colony,allrd2, by="COLONYID")
+  long <- gather(allrd3, RDCond, abun, names(allrd3[22:dim(allrd3)[2]]), factor_key=TRUE) #convert wide to long format by condition
+  long$GROUP<-long[,grouping_field]
+  longsum<-ddply(long, .(SITE,SITEVISITID,TRANSECT,GROUP,RDCond), #calc total colonies by taxon and condition
+                 summarise,
+                 RDabun=sum(abun))
+  out1<-ddply(longsum, .(SITE,SITEVISITID,TRANSECT,RDCond), #calc total colonies by condition
+              summarise,
+              RDabun=sum(RDabun))
+  out1$GROUP<-"SSSS"; out1 <- out1[c(1,2,3,6,4,5)] #add total colony code
+  a<-rbind(longsum,out1)
+  a<-subset(a,RDCond!="NONE")
+  
+  #Convert back to wide format
+  abun<-dcast(a, formula=SITEVISITID +SITE + TRANSECT+GROUP~ RDCond, value.var="RDabun",sum,fill=0)
+  
+  trarea<-Calc_SurveyArea_By_Transect(data) #calculate survey area/site
+  
+  #merge dataframes
+  ab.tr<-merge(trarea,abun,by=c("SITEVISITID","SITE","TRANSECT"),all=TRUE)
+  ab.tr[is.na(ab.tr)]<-0
+  new_DF <- ab.tr[rowSums(is.na(ab.tr)) > 0,] #identify which rows have NAs
+  
+  
+  #calcualte density of each condition
+  cd<-ab.tr[, 6:ncol(ab.tr)]/ab.tr$TRANSECTAREA # selects every row and 2nd to last columns
+  out<-cbind(ab.tr[,c(1:5)],cd) #cbind the transect info to data.
+  
+  colnames(out)[which(colnames(out) == 'GROUP')] <- grouping_field #change group to whatever your grouping field is.
+  
+  return(out)
+}
 
 #This function calculate abundance of conditions conditions by transect and taxonomic group
 #Transform RD1 and RD2 from long to wide format, remove other site AND TRANSECT level info
@@ -243,7 +312,7 @@ Calc_Condden_Transect<-function(data, grouping_field="S_ORDER"){
   scl<-subset(data,COLONYLENGTH>5&S_ORDER=="Scleractinia"&Adult_juv=="A")
   
   c<-dcast(scl, formula=SITEVISITID + SITE+TRANSECT+COLONYID ~ COND, value.var="COND",length,fill=0)
-  c$ChronicDZ<-c$PDS+c$FUG+c$SGA
+  c$ChronicDZ<-c$PDS+c$FUG+c$SGA+c$PTR
   c <- subset(c, select = -c(NDZ)) #remove columns
   a<-merge(survey_colony,c, by=c("SITE","SITEVISITID","TRANSECT","COLONYID"))
   long <- gather(a, COND, Condabun, names(a[22:dim(a)[2]]), factor_key=TRUE) #convert wide to long format by condition
@@ -258,7 +327,7 @@ Calc_Condden_Transect<-function(data, grouping_field="S_ORDER"){
               Condabun=sum(Condabun))
   out1$GROUP<-"SSSS"; out1 <- out1[c(1,2,3,6,4,5)] #add total colony code
   a<-rbind(longsum,out1)
-  a<-subset(a,COND!="NODATA")
+  a<-subset(a,COND!="NONE")
   
 
   #convert wide to long
@@ -374,7 +443,7 @@ Calc_RDden_Site<-function(data, grouping_field="S_ORDER"){
   
   #Sum identically named columns and remove the no data column
   allrd2<-as.data.frame(sapply(unique(colnames(allrd)), 
-                               function(x) rowSums(allrd[, colnames(allrd) == x, drop = FALSE])));allrd2<-allrd2[,!(colnames(allrd2) =="All_NODATA")]
+                               function(x) rowSums(allrd[, colnames(allrd) == x, drop = FALSE])));allrd2<-allrd2[,!(colnames(allrd2) =="All_NONE")]
   
   #merge data with colony level metadata and sum conditions by transect and taxoncode
   allrd3<-merge(survey_colony,allrd2, by="COLONYID")
@@ -388,7 +457,7 @@ Calc_RDden_Site<-function(data, grouping_field="S_ORDER"){
               RDabun=sum(RDabun))
   out1$GROUP<-"SSSS"; out1 <- out1[c(1,2,5,3,4)] #add total colony code
   a<-rbind(longsum,out1)
-  a<-subset(a,RDCond!="NODATA")
+  a<-subset(a,RDCond!="NONE")
   
   #Convert back to wide format
   abun<-dcast(a, formula=SITEVISITID +SITE + GROUP~ RDCond, value.var="RDabun",sum,fill=0)
@@ -407,6 +476,72 @@ Calc_RDden_Site<-function(data, grouping_field="S_ORDER"){
   
   return(out)
 }
+
+#MESSING WITH FUNCTION TO MAKE IT MORE EFFICENT
+#This function calculate abundance  then the density of recent dead conditions by transect and taxonomic group
+#Transform RD1 and RD2 from long to wide format, remove other site AND TRANSECT level info
+
+Calc_RDden_Site<-function(data, grouping_field="S_ORDER"){
+  #Remove scleractinan adult colony fragments
+  scl<-subset(data,COLONYLENGTH>5&S_ORDER=="Scleractinia")
+  
+
+  names(scl)<-gsub("DZGN","DZGNS",names(rd1),fixed = TRUE)
+  
+  rd1<-dcast(scl, formula=SITEVISITID + SITE+COLONYID ~ RD1, value.var="RD1",length,fill=0);names(rd1)<-gsub("DZGN","DZGNS",names(rd1),fixed = TRUE)#; rd1<-rd1[,-c(1:3)]
+  rd2<-dcast(scl, formula=SITEVISITID + SITE+COLONYID ~ RD2, value.var="RD2",length,fill=0) ;names(rd2)<-gsub("DZGN","DZGNS",names(rd2),fixed = TRUE)#; rd2<-rd2[,-c(1:3)]
+  rd3<-dcast(scl, formula=SITEVISITID + SITE+COLONYID ~ GENRD1, value.var="GENRD1",length,fill=0);colnames(rd3)[4:ncol(rd3)]<-paste("All",colnames(rd3[,c(4:ncol(rd3))]),sep="_") 
+  rd4<-dcast(scl, formula=SITEVISITID + SITE+COLONYID ~ GENRD2, value.var="GENRD2",length,fill=0);colnames(rd4)[4:ncol(rd4)]<-paste("All",colnames(rd4[,c(4:ncol(rd4))]),sep="_") 
+  
+  #merge all dataframes together
+  a<-merge(rd1,rd2,by=c("SITEVISITID","SITE","COLONYID"),all=TRUE)
+  b<-merge(a,rd3,by=c("SITEVISITID","SITE","COLONYID"),all=TRUE)
+  allrd<-merge(b,rd4,by=c("SITEVISITID","SITE","COLONYID"),all=TRUE)
+  
+  allrd<-allrd[,-c(1:2)] #remove all metadata except colonyid
+  
+  #R will add .x and .y to column names because there are columns that are identically names
+  # this will remove .x and .y so that we can sum identifically named columns
+  names(allrd)<-gsub(".x","",names(allrd),fixed = TRUE)
+  names(allrd)<-gsub(".y","",names(allrd),fixed = TRUE)
+  head(allrd)
+  
+  #Sum identically named columns and remove the no data column
+  allrd2<-as.data.frame(sapply(unique(colnames(allrd)), 
+                               function(x) rowSums(allrd[, colnames(allrd) == x, drop = FALSE])));allrd2<-allrd2[,!(colnames(allrd2) =="All_NONE")]
+  
+  #merge data with colony level metadata and sum conditions by transect and taxoncode
+  allrd3<-merge(survey_colony,allrd2, by="COLONYID")
+  long <- gather(allrd3, RDCond, abun, names(allrd3[22:dim(allrd3)[2]]), factor_key=TRUE) #convert wide to long format by condition
+  long$GROUP<-long[,grouping_field]
+  longsum<-ddply(long, .(SITE,SITEVISITID,GROUP,RDCond), #calc total colonies by taxon and condition
+                 summarise,
+                 RDabun=sum(abun))
+  out1<-ddply(longsum, .(SITE,SITEVISITID,RDCond), #calc total colonies by condition
+              summarise,
+              RDabun=sum(RDabun))
+  out1$GROUP<-"SSSS"; out1 <- out1[c(1,2,5,3,4)] #add total colony code
+  a<-rbind(longsum,out1)
+  a<-subset(a,RDCond!="NONE")
+  
+  #Convert back to wide format
+  abun<-dcast(a, formula=SITEVISITID +SITE + GROUP~ RDCond, value.var="RDabun",sum,fill=0)
+  
+  trarea<-Calc_SurveyArea_By_Site(data) #calculate survey area/site
+  
+  #merge dataframes
+  ab.tr<-merge(trarea,abun,by=c("SITEVISITID","SITE"),all=TRUE)
+  ab.tr[is.na(ab.tr)]<-0
+  
+  #calcualte density of each condition
+  cd<-ab.tr[, 5:ncol(ab.tr)]/ab.tr$TRANSECTAREA # selects every row and 2nd to last columns
+  out<-cbind(ab.tr[,1:4],cd)
+  
+  colnames(out)[which(colnames(out) == 'GROUP')] <- grouping_field #change group to whatever your grouping field is.
+  
+  return(out)
+}
+
 
 
 #This function calculate abundance then density of conditions conditions by transect and taxonomic group
@@ -530,11 +665,12 @@ Calc_Domain=function(site_data,grouping_field="S_ORDER",metric_field=c("AdColDen
                     Y._st=sum(Y._h,na.rm=TRUE), #Domain total abundance (sum of extrapolated strata abundance)
                     varY._st=sum(varY._h,na.rm=TRUE),#Domain variance total abundance (sum of extrapolated strata varaiance abundance)
                     n=sum(n_h,na.rm=TRUE), #total sites surveyed in domain
-                    N=sum(N_h,na.rm=TRUE), #total possible sites in domain
-                    SE_varD._st=sqrt(varD._st), #SE of domain metric estimate
-                    CV_varD._st=SE_varD._st/D._st, #CV of domain metric estimate
-                    SE_varY._st=sqrt(varY._st),#SE of domain abundance estimate
-                    CV_varY._st=SE_varY._st/Y._st)#CV of domain abundnace estimate
+                    Ntot=sum(N_h,na.rm=TRUE), #total possible sites in domain
+                    SE_D._st=sqrt(varD._st), #SE of domain metric estimate
+                    CV_D._st=SE_varD._st/D._st, #CV of domain metric estimate
+                    SE_Y._st=sqrt(varY._st),#SE of domain abundance estimate
+                    CV_Y._st=SE_varY._st/Y._st.)#CV of domain abundnace estimate
+      #Add Weighted proportion occurance, se and cv)
   
   colnames(Domain_roll)[which(colnames(Domain_roll) == 'GROUP')] <- grouping_field #change group to whatever your grouping field is.
   
