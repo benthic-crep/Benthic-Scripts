e1d64e076f5d98d96db6c91f67e87e5f00efde0c
Camir314 <59746027+Camir314@users.noreply.github.com>
2020-02-24 17:18:21 -1000

    Making QC script for v2 geodatabase
    Fixed columns with missing values, removed checks related to calibration and comparsion studies

--- SfM/SfMdemog_QC_v2.R
+++ SfM/SfMdemog_QC_v2.R
@@ -1,10 +1,6 @@
 #This script QCs the SfM data pulled directly from the geodatabse
 #It is designed to merge the geodatabase with the site visit table and help flag errors to be corrected 
-#Completed by Corinne Amir 12/18/2019
-#updated by Courtney Couch 1/2/2020
 
-
-rm=ls() #removes all objects from the current workspace
 
 setwd("T:/Benthic/Data/SfM/QC")
 
@@ -17,68 +13,78 @@ sfm.raw <- read.csv("HARAMP2019_demographic_v2_feb192020.csv") #for comparison (
 
 head(sfm.raw);nrow(sfm.raw)
 
-
+setwd("C:/Users/Corinne.Amir/Documents/GitHub/Benthic-Scripts/SfM") #export csv files here if/when T drive doesn't give permission
 
 # Prepping The Data -----------------------------------------------------
 
-#Turn "no" into 0
-sfm.raw$NO_COLONY <- ifelse(sfm.raw$NO_COLONY == "No", "0", as.character(sfm.raw$NO_COLONY))
-sfm.raw$EX_BOUND <- ifelse(sfm.raw$EX_BOUND == "No", "0", as.character(sfm.raw$EX_BOUND))
-sfm.raw$JUVENILE <- ifelse(sfm.raw$JUVENILE == "No", "0", as.character(sfm.raw$JUVENILE))
-sfm.raw$FRAGMENT <- ifelse(sfm.raw$FRAGMENT == "No", "0", as.character(sfm.raw$FRAGMENT))
-sfm.raw$REMNANT <- ifelse(sfm.raw$REMNANT == "No", "0", as.character(sfm.raw$REMNANT))
+# #Turn "no" into 0
+# sfm.raw$NO_COLONY <- ifelse(sfm.raw$NO_COLONY == "No", "0", as.character(sfm.raw$NO_COLONY))    #if below function is okay, this isnt necessary
+# sfm.raw$EX_BOUND <- ifelse(sfm.raw$EX_BOUND == "No", "0", as.character(sfm.raw$EX_BOUND))
+# sfm.raw$JUVENILE <- ifelse(sfm.raw$JUVENILE == "No", "0", as.character(sfm.raw$JUVENILE))
+# sfm.raw$FRAGMENT <- ifelse(sfm.raw$FRAGMENT == "No", "0", as.character(sfm.raw$FRAGMENT))
+# sfm.raw$REMNANT <- ifelse(sfm.raw$REMNANT == "No", "0", as.character(sfm.raw$REMNANT))
+
+
+#Fix other columns...HARAMP 2019 specific!
+sfm.raw$OBS_YEAR <- rep(2019, times=nrow(sfm.raw))
+sfm.raw$MISSION_ID <- rep("SE1902", times=nrow(sfm.raw))
+sfm.raw$NO_COLONY <- as.factor(ifelse(sfm.raw$NO_COLONY != "-1", "0", as.character(sfm.raw$NO_COLONY))) #iNO_COLONY can be left as NA. Really just have to make sure that you put -1 when there are no colonies. Writing 0 is equivalent to NA
+sfm.raw$REMNANT <- as.factor(ifelse(sfm.raw$REMNANT != "-1", "0", as.character(sfm.raw$REMNANT))) # If no_colony, juvenile, frag, remnant missing just assume they = 0
+sfm.raw$JUVENILE <- as.factor(ifelse(sfm.raw$JUVENILE != "-1", "0", as.character(sfm.raw$JUVENILE))) 
+sfm.raw$EX_BOUND <- as.factor(ifelse(sfm.raw$EX_BOUND != "-1", "0", as.character(sfm.raw$EX_BOUND))) 
+sfm.raw$FRAGMENT <- as.factor(ifelse(sfm.raw$FRAGMENT != "-1", "0", as.character(sfm.raw$FRAGMENT))) 
+sfm.raw <- sfm.raw %>% mutate_at(vars(RD_1, RD_2, RD_3, SEV_1, SEV_2, SEV_3,EXTENT_1, EXTENT_2, EXTENT_3, SEGLENGTH, SEGWIDTH), ~replace(., is.na(.), 0))
+sfm.raw <- sfm.raw %>% mutate_at(vars(RDCAUSE1, RDCAUSE2, RDCAUSE3, CON_1, CON_2, CON_3, SITE, ANALYST, SPCODE, MORPH_CODE), ~recode(., "0" = "NA", " "="NA"))
+
+sfm.raw <- droplevels(sfm.raw)
+sapply(sfm.raw,unique)
 
 
 
-#### If some column classes = logical, Run this function that removes logical NAs ####
-RemoveLogicalNA <- function(b)
-{	
-  if (is.logical(b) == "TRUE") {   
-    b[is.logical(b)] <- "NA"
-    b <- as.factor(b)
-  }
-  return(b)
-}
-#### end function ####
+#Create a dataframe that houses all rows that have not been completely filled out (not including RD and CON-related columns)
+sfm.raw$ANALYST <- as.factor(sfm.raw$ANALYST)
+sfm.raw$SITE <- as.factor(sfm.raw$SITE)
 
-#Add "NA" to columns with class = logical (whole column filled with italized, shaded "NA")
-sfm.raw$RDCAUSE3 <- RemoveLogicalNA(sfm.raw$RDCAUSE3)
-sfm.raw$CON_3 <- RemoveLogicalNA(sfm.raw$CON_3)
+sfm.missing.duplicate.rows <- rbind(
+  analyst.missing <- filter(sfm.raw, ANALYST %in% c("NA", " ")),
+  site.missing <- filter(sfm.raw, SITE %in% c("NA-   NA", "-   NA", "A-   NA")),
+  seglength.missing <-filter(sfm.raw, SEGLENGTH %in% c(0.0, "NA")),
+  segwidths.missing <- filter(sfm.raw, SEGWIDTH %in% c("0", "NA")),
+  spcode.missing <-  filter(sfm.raw, SPCODE %in% c("NA")),
+  morphcode.missing <-  filter(sfm.raw, MORPH_CODE %in% c("NA")),
+  transect.missing <-  filter(sfm.raw, TRANSECT %in% c(0, "<Null>", " ")),
+  segmennt.missing <-  filter(sfm.raw, SEGMENT %in% c("NA", "<NA>")))
 
-
-
-#Remove turn blank cells into NA or 0 depending on column class
-sfm.factor <- sfm.raw[,sapply(sfm.raw, is.factor)]
-sfm.num <- sfm.raw[,sapply(sfm.raw, is.integer)]
-sfm.num <- cbind(sfm.num, (sfm.raw[,sapply(sfm.raw, is.character)]))
+sfm.missing <- sfm.missing.duplicate.rows[!duplicated(sfm.missing.duplicate.rows),] 
+dim(sfm.missing)
+View(sfm.missing)
 
 
-#Turn blanks into NA...not working
-b <- ifelse(sfm.raw == "", "NA", as.factor(sfm.raw))
+#Identify all rows where NO_COLONY_ is -1 and all values beforehand are also filled in. These values are ok and should NOT be placed in the sfm.missing dataframe
+no.colony.present <- sfm.missing %>%
+  filter(NO_COLONY == "-1" & ANALYST != "NA" & SITE != "NA" & SEGLENGTH != "0" & SEGWIDTH != "0")
+head(no.colony.present)
 
-a <- function(data) {
-  for(i in ncol(data)) {
-    ifelse(data[i] == "", "NA", as.factor(i))
-  }
-  # for(i in ncol(data)) {
-  #   ifelse(is.factor(i)== T & data[i] == " ", "NA", as.factor(i))
-  # }
-  data <-droplevels(data) 
-  return(data)
-}
- 
-sfm.raw <- a(sfm.raw)
 
-sfm.raw[sfm.raw == " "] <- "NA"
-sfm.raw[sfm.raw == "" & is.factor == T] <- "NA"
-sfm.raw<-droplevels(sfm.raw)
-str(sfm.raw)
+#Remove rows with no colony present from the sfm.missing dataframe
+sfm.missing$SEGMENT <- as.factor(sfm.missing$SEGMENT)
+sfm.missing$SITE <- as.factor(sfm.missing$SITE)
+sfm.missing <- droplevels(anti_join(sfm.missing, no.colony.present))
 
-#Fix columns that contain both NA and 0
-sfm.raw <- as.factor(c(sfm.raw$CON_3, sfm.raw$CON_2)) #columns that should have integers
-#columns that should have factors
-#If column is a factor, turn 0 into NA NAs found alongsize 0 in SEV, EXTENT, 
-#If column is a factor, turn 0 into NA NAs found alongsize 0 in SEV, EXTENT, 
+#Save dataframe with missing values 
+write.csv(sfm.missing, "sfm_missing_rows.csv") #get these rows repopulated (if missing metadata) or annotated before moving forward
+
+
+#If charging forward and leaving rows with missing data behind, create a new dataframe where all rows with missing data have been removed
+sfm.raw$SEGMENT <- as.factor(sfm.raw$SEGMENT)
+sfm.raw$SITE <- as.factor(sfm.raw$SITE)
+sfm <- droplevels(anti_join(sfm.raw, sfm.missing))
+
+View(sfm)
+nrow(sfm)
+
+sfm$SEGMENT <- as.factor(sfm$SEGMENT)
+sfm$SITE <- as.factor(sfm$SITE)
 
 
 #### Run sfm-specific SiteNumLeadingZeros function - we needed to modify this from the Core functions script because we have both - and _ in site name ####
@@ -108,50 +114,31 @@ SiteNumLeadingZeros_SfM <- function(site_names)
 #### end function ####
 
 # Change site numbers such as MAR-22 to MAR-0022
-sfm.raw$SITE<-SiteNumLeadingZeros_SfM(sfm.raw$SITE)
+sfm$SITE<-SiteNumLeadingZeros_SfM(sfm$SITE)
 
-sfm.raw$SEGMENT<-as.factor(sfm.raw$SEGMENT)
-table(sfm.raw$SITE,sfm.raw$SEGMENT)
-
-
-#Create a dataframe that houses all rows that have not been completely filled out (not including RD and CON-related columns)
-sfm.missing.duplicate.rows <- rbind(
-  analyst.missing <- filter(sfm.raw, ANALYST %in% c("NA")),
-  site.missing <- filter(sfm.raw, SITE %in% c("NA")),
-  seglength.missing <-filter(sfm.raw, SEGLENGTH %in% c("0")),
-  segwidths.missing <- filter(sfm.raw, SEGWIDTH %in% c("0")),
-  spcode.missing <-  filter(sfm.raw, SPCODE %in% c("NA")),
-  morphcode.missing <-  filter(sfm.raw, MORPH_CODE %in% c("NA"))) 
-
-sfm.missing <- sfm.missing.duplicate.rows[!duplicated(sfm.missing.duplicate.rows),] 
-dim(sfm.missing)
-View(sfm.missing)
+sfm$SEGMENT<-as.factor(sfm$SEGMENT)
+table(sfm$SITE,sfm$SEGMENT)
 
 
-#Identify all rows where NO_COLONY_ is -1 and all values beforehand are also filled in. These values are ok and should NOT be placed in the sfm.missing dataframe
-no.colony.present <- sfm.missing %>%
-  filter(NO_COLONY == "-1" & ANALYST != "NA" & SITE != "NA" & SEGLENGTH != "0" & SEGWIDTH != "0")
-head(no.colony.present)
 
+#### If some column classes = logical, Run this function that removes logical NAs ####
+RemoveLogicalNA <- function(b)
+{	
+  if (is.logical(b) == "TRUE") {   
+    b[is.logical(b)] <- "NA"
+    b <- as.factor(b)
+  }
+  return(b)
+}
+#### end function ####
 
-#Remove rows with no colony present from the sfm.missing dataframe
-sfm.missing <- droplevels(anti_join(sfm.missing, no.colony.present))
+#Add "NA" to columns with class = logical (whole column filled with italized, shaded "NA")
+sfm$RDCAUSE3 <- is.logical(sfm$RDCAUSE3) #whole column shows up as shaded but is treated as character
+sfm$CON_3 <- is.logical(sfm$CON_3) 
+sfm$RDCAUSE3 <- RemoveLogicalNA(sfm$RDCAUSE3)
+sfm$CON_3 <- RemoveLogicalNA(sfm$CON_3)
+sfm$SEV_3 <- RemoveLogicalNA(sfm$SEV_3)
 
-sfm.missing$SEGMENT <- as.factor(sfm.missing$SEGMENT)
-sfm.missing$SITE <- as.factor(sfm.missing$SITE)
-
-#Save dataframe with missing values 
-write.csv(sfm.missing, "sfm_missing_rows.csv") #get these rows repopulated (if missing metadata) or annotated before moving forward
-
-
-#If charging forward and leaving rows with missing data behind, create a new dataframe where all rows with missing data have been removed
-sfm <- droplevels(anti_join(sfm.raw, sfm.missing))
-
-View(sfm)
-nrow(sfm)
-
-sfm$SEGMENT <- as.factor(sfm$SEGMENT)
-sfm$SITE <- as.factor(sfm$SITE)
 
 
 #Add column for segment area
@@ -167,7 +154,7 @@ sfm$SEGAREA <- sfm$SEGLENGTH*sfm$SEGWIDTH
 
 #How many site/segments were annotated 
 sfm$site_seg<-paste(sfm$SITE,sfm$SEGMENT)
-length(unique(sfm$site_seg)) #Feb 19 = 124 unique sites
+length(unique(sfm$site_seg)) #Feb 19 = 122 unique sites
 
 # QC Checks ---------------------------------------------------------------
 #Set up output csv file that reports the status of the qc checks
@@ -193,22 +180,22 @@ write.csv(partial_SiteSeg_removal, "Error_partial_filled_segments.csv")
 
 
 
-#2.Check that the columns have the appropripate type of data (e.g. numeric vs. text) & no errant codes (e.g. ble instead of BLE) 
+#2.Check that the columns have the appropripate type of data (e.g. numeric vs. text) & no errant codes (e.g. SEV and/or RD columns contain NA) 
 sapply(sfm,unique)
 str(sfm) 
 sapply(sfm, class)
 
-output[2,]<-c("No errant codes","Yes") #change depending on output from previous lines of code
+output[2,]<-c("No errant codes","YES") #change depending on output from previous lines of code
 
 
 
 #3. All TRANSECT within v2 of the geodabase should = A (B is for repeats)
-levels(sfm$TRANSECT)
+filter(sfm, TRANSECT != "A")
 
 output[3,]<-c("All transects = A","Yes") #change depending on output from previous lines of code
 
 
-#3. Make sure that if NO_COLONY=-1 none of the following columns have been populated
+#4. Make sure that if NO_COLONY=-1 none of the following columns have been populated
 sfm %>% filter(sfm$SPCODE == "NA" & sfm$NO_COLONY != -1)
 sfm %>% filter(sfm$SPCODE != "NA" & sfm$NO_COLONY == -1)
 sfm %>% filter(sfm$SPCODE == -1 & sfm$NO_COLONY == -1)
@@ -218,81 +205,57 @@ sfm %>% filter(sfm$REMNANT == -1 & sfm$NO_COLONY == -1)
 sfm %>% filter(sfm$MORPH_CODE == "NA" & sfm$NO_COLONY != -1)
 sfm %>% filter(sfm$MORPH_CODE != "NA" & sfm$NO_COLONY == -1)
 
-output[3,]<-c("NO_COLONY segments filled correctly","Yes") 
+output[4,]<-c("NO_COLONY segments filled correctly","HAW-4287 Juvenile and NO_colony = -1") 
 
 
 
-#3. Calculate the number of annotated segments per site
+#5. Calculate the number of annotated segments per site and check that all segments contain both seglengths (except segment 15)
 ##Create a summary table of #segments per site and check against tracking data sheet
 seg.per.site <- ddply(sfm,.(SITE, SEGMENT), summarize, num.annotated = n_distinct(SEGMENT))
 eval.seg.per.site <- as.data.frame(acast(seg.per.site, SITE~SEGMENT, length))
-eval.seg.per.site$Total <- rowSums(eval.seg.per.site)
+#eval.seg.per.site$Total <- rowSums(eval.seg.per.site)
 View(eval.seg.per.site) 
 
 #use this file to evaluate where segments may be missing
 write.csv(eval.seg.per.site, "Missing_seg_eval.csv")
 
-output[3,]<-c("All segments within each site have been annotated","Some sites have unannotated segments -- ok") #change depending on output from previous line of code
+output[5,]<-c("All annotated segments have two seglengths","Multiple errors, see csv") #change depending on output from previous line of code
+
+
+
+
+#6. Make sure only 1 annotator exists per site
+analyst.per.site <- ddply(sfm,.(SITE), summarize, num.analyst = n_distinct(ANALYST))
+filter(analyst.per.seg, num.analyst>1)
+
+output[6,]<-c("All sites annotated by one person","YES")
 
 
 
 
 
-#4. Check how many anotators exist for each segment within a site 
-analyst.per.seg<-sfm %>% filter(ANALYST=="RS" | ANALYST=="MW" | ANALYST=="MA") #for comparison plots NOT calibration plots
-analyst.per.seg <- ddply(sfm,.(SITE, SEGMENT), summarize, num.analyst = n_distinct(ANALYST))
-analyst.multiple <- filter(analyst.per.seg, num.analyst>1)
-analyst.multiple.names <- left_join(analyst.multiple, sfm[,1:6]) 
-analyst.multiple.names <- analyst.multiple.names %>% unite(JOIN, SITE, SEGMENT, sep = "--")
-analyst.multiple.names <-ddply(analyst.multiple.names,.(JOIN, ANALYST), summarize, num.colonies = n_distinct(FID))
-View(analyst.multiple.names)
 
-#use this file to evaluate where we are or are not expecting there to be additional analysts
-write.csv(analyst.multiple.names, "Duplicate_analyst_eval.csv") 
-
-output[4,]<-c("All segments have been annotated by one individuals","Multiple segments with > 1 annotator -- ok") #change depending on output from previous line of code
-
-#Jan17=44 site-segs with at least 2 annotators
-
-
-
-
-#5.Check for incorrect species-V:\PHOTOMOSAIC (1)\HARAMP\HARAMP_2019_codes.csv
+#7.Check for incorrect species-V:\PHOTOMOSAIC (1)\HARAMP\HARAMP_2019_codes.csv
 ddply(sfm,.(SPCODE),summarize,temp=length(SPCODE))
 
-output[5,]<-c("Species codes are correct","OK -- although several species present that aren't scored to species, only genus")
+output[7,]<-c("Species codes are correct","OK -- although several species present that aren't scored to species, only genus")
 
 
 
 
 
-#6. Check that SEGWIDTH is correct (should have been apparent in qc #1).
+#8. Check that SEGWIDTH is correct (should have been apparent in qc #1).
 levels(as.factor(sfm$SEGAREA)) #should all be 1 OR 2.5, unless otherwise stated 
 
-output[6,]<-c("All segment widths are correct","Yes") #change depending on output from previous line of code
-
-
-# Check if all segments contain both seglengths 
-seg.length <-ddply(sfm,.(SITE, SEGMENT, ANALYST, SEGLENGTH), summarize, num.seglengths = n_distinct(SEGLENGTH)) 
-eval.seg.length <- acast(seg.length, SITE~SEGMENT~ANALYST, length)   
-View(eval.seg.length) #all cells that have been annotated should be "2" 
-
-output[7,]<-c("All segments have seglengths 1.0 and 2.5","No errors in MA, ML, RS--ok") #change depending on output from previous line of code
-
-# if there are errors, export a csv file for further analysis
-write.csv(eval.seg.length, "Missing_seglength_eval.csv")
+output[8,]<-c("All segment widths are correct","Yes") #change depending on output from previous line of code
 
 
 
+#9. Identify colonies flagged as Juveniles or Adults, but have the innocorrect segment area. make sure j = 1 and A = 2.5
+sm.colonies.eval <- sfm %>% filter(JUVENILE== -1,SEGAREA != 1); sm.colonies.eval
+lg.colonies.eval <- sfm %>% filter(JUVENILE==0,SEGAREA==1, NO_COLONY==0); lg.colonies.eval
 
-#8. Identify colonies flagged as Juveniles or Adults, but have the innocorrect segment area. make sure j = 1 and A = 2.5
-sm.colonies.eval <- sfm %>% filter(Adult_Juvenile=="J",SEGAREA != 1, NO_COLONY_ !=-1) 
-View(sm.colonies.eval)
-
-lg.colonies.eval <- sfm %>% filter(Adult_Juvenile=="A",SEGAREA==1) 
-View(lg.colonies.eval)
-
-output[8,]<-c("Juveniles and Adult colonies have correct labeling","2 errors (1 PBER)")
+output[9,]<-c("Juveniles and Adult colonies have correct labeling","Multiple errors, see csv")
 
 #If rows have been flagged, export sm_colonies dataframe into a csv file for further QC
 write.csv(sm.colonies.eval, "Juveniles_eval.csv")
@@ -300,21 +263,21 @@ write.csv(lg.colonies.eval, "Adults_eval.csv")
 
 
 
-#9. Identify colonies with 0% recent dead, but has an RDCAUSE code - This check should result in 0 records   
+#10. Identify colonies with 0% recent dead, but has an RDCAUSE code - This check should result in 0 records   
 sfm[sfm$RD_1=="0"& sfm$RDCAUSE1!="NA",]
 sfm[sfm$RD_2=="0"& sfm$RDCAUSE2!="NA",]
 sfm[sfm$RD_3=="0"& sfm$RDCAUSE3!="NA",]
 
-output[9,]<-c("0% Recent Dead corals do NOT have an RDCAUSE code","Errors for AH--ok")
+output[10,]<-c("0% Recent Dead corals do NOT have an RDCAUSE code","YES")
 
 
 
-#10. Identify colonies with recent dead >0%, but there is no RDCAUSE code - This check should result in 0 records   
+#11. Identify colonies with recent dead >0%, but there is no RDCAUSE code - This check should result in 0 records   
 sfm[sfm$RD_1 >0 & sfm$RDCAUSE1=="NA",] #,rowSums(is.na(sfm)) != ncol(sfm),]
 sfm[sfm$RD_2 >0 & sfm$RDCAUSE2=="NA",] #,rowSums(is.na(a)) != ncol(a), ]
 sfm[sfm$RD_3 >0 & sfm$RDCAUSE3=="NA",] #,rowSums(is.na(a)) != ncol(a), ]
 
-output[10,]<-c("All corals with RD >0 have an RDCAUSE code","YES")
+output[11,]<-c("All corals with RD >0 have an RDCAUSE code","YES")
 
 
 
@@ -328,20 +291,20 @@ output[11,]<-c("All colonies with a condition have an extent","Errors for AH--ok
 
 
 #12. Identify colonies that have no condition, but a value in extent - This check should result in 0 records   
-sfm[sfm$CON_1=="NA"& sfm$EXTENT_1!="0",] #rowSums(is.na(a)) != ncol(a),]
+sfm[sfm$CON_1=="NA"& sfm$EXTENT_1!="0",] 
 sfm[sfm$CON_2=="NA"& sfm$EXTNET_2!="0",]
 sfm[sfm$CON_3=="NA"& sfm$EXTENT_3!="0",] #rowSums(is.na(a)) != ncol(a),]
 
-output[12,]<-c("All colonies with NO condition also have NO extent","Yes")
+output[12,]<-c("All colonies with NO condition also have NO extent","MAI-2567 has extent and severity, but no condition")
 
 
 
 #13. Identify colonies with nothing in condition column, but a value in severity. Double check that these shouldn't be 0  
-sfm[sfm$EXTENT_1=="0"& sfm$SEV_1!="0"] #,rowSums(is.na(a)) != ncol(a),]
+sfm[sfm$EXTENT_1=="0"& sfm$SEV_1!="0",] #,rowSums(is.na(a)) != ncol(a),]
 sfm[sfm$EXTENT_2=="0"& sfm$SEV_2!="0",]
 sfm[sfm$EXTENT_3=="0"& sfm$SEV_3!="0",]
 
-output[13,]<-c("Identify colonies with nothing in condition column, but a value in severity","Yes")
+output[13,]<-c("All colonies with NO extent have NO severity","Yes")
 
 
 
@@ -355,7 +318,7 @@ sfm[sfm$SEV_1!="0"& sfm$CON_1 %notin% c("BLE","BLP"),]
 sfm[sfm$SEV_2!="0"& sfm$CON_2 %notin% c("BLE","BLP"),]
 sfm[sfm$SEV_3!="0"& sfm$CON_3 %notin% c("BLE","BLP"),]
 
-output[14,]<-c("Severity value is present only in colonies with CON = BLE and BLP","Errors for AH--ok")
+output[14,]<-c("Severity value is present only in colonies with CON = BLE and BLP","MAI-2567 same issue noted in #12")
 
 
 
@@ -371,19 +334,13 @@ output[15,]<-c("RD + OD <=100%","Yes")
 
 
 
-#16. Write files for qc #9-15 that have errors (optional)
-SEV_1_error <- sfm[sfm$SEV_1=="0"& sfm$CON_1 %in% c("BLE","BLP"),]
-write.csv(SEV_1_error, "SEV_1_error.csv") 
-
-
-
 #Export QC output table with appropriate file name
 setwd("C:/Users/Corinne.Amir/Documents/GitHub/Benthic-Scripts/SfM")
 write.csv(output,"HARAMP2019_sfm_output.csv")
 
 
 
-# Export QC'd data ----------------------------------------------------------------------------
+# Export QC'd data...not edited from v1 ----------------------------------------------------------------------------
 
 ad<-subset(sfm,Adult_Juvenile=="A"|NO_COLONY_==-1 & SEGLENGTH==2.5)
 ad<-subset(ad,select=-c(Adult_Juvenile,totaldead))
